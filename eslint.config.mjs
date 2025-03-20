19d2a741d4e543faad043dc0a9e56a6a5e405299
tnantoka <tnantoka@bornneet.com>
2025-03-20 16:06:28 +0900

    feat: add prettier/eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,9 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+  eslintConfigPrettier,
+);
--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts']
-};
